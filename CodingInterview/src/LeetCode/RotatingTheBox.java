7406b6f042e9869877969b73154f25853bf2749f
Scott Urman <scott.d.urman@gmail.com>
2023-08-01 09:13:47 -0700

    Rotate the box

--- CodingInterview/src/LeetCode/RotatingTheBox.java
+++ CodingInterview/src/LeetCode/RotatingTheBox.java
@@ -2,90 +2,86 @@ package LeetCode;
 
 // https://leetcode.com/problems/rotating-the-box/
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class RotatingTheBox {
   private static final char STONE = '#';
   private static final char OBSTACLE = '*';
   private static final char EMPTY = '.';
 
   static char[][] rotate(char[][] box) {
-    int inputXLength = box[0].length;
-    int inputYLength = box.length;
-    System.out.println("inputXLength: " + inputXLength);
-    System.out.println("inputYLength: " + inputYLength);
+    int numRows = box.length;
+    int numColumns = box[0].length;
+    char[][] retVal = new char[numColumns][numRows];
 
-    // [0,0] -> [4,0]
-    // [1,0] -> [4,1]
-    // [2,0] -> [4, 2]
-    // [0,1] -> [3, 0]
-
-    char[][] rotatedBox = new char[inputYLength][inputXLength];
-    for (int x = 0; x < inputXLength; x++) {
-      for (int y = 0; y < inputYLength; y++) {
-        int rotatedX = (y + inputYLength) % inputYLength;
-        int rotatedY = x;
-
-        System.out.printf("Setting rotatedBox[%d][%d] = box[%d][%d] %c\n", rotatedX, rotatedY, x, y, box[x][y]);
-        rotatedBox[rotatedX][rotatedY] = box[x][y];
+    // [r,c] -> [c, numRows - 1 - r]
+    for (int row = 0; row < numRows; row++) {
+      for (int column = 0; column < numColumns; column++) {
+        retVal[column][numRows - 1 - row] = box[row][column];
       }
     }
-
-    return rotatedBox;
+    return retVal;
   }
 
   static void gravity(char [][] matrix) {
-    System.out.println("matrix.length: " + matrix.length);
-    for (int x = 0; x < matrix[0].length; x++) {
-      int currentTop = matrix.length - 1;
-      System.out.println("matrix[x].length: " + matrix[x].length);
+    int numRows = matrix.length;
+    int numColumns = matrix[0].length;
 
-      for (int y = matrix.length - 1; y >=0; y--) {
-        //  System.out.printf("Considering matrix[%d][%d] = %c\n%n", x, y, matrix[y][x]);
-
-        switch (matrix[y][x]) {
-          case ' ':
+    for (int column = 0; column < numColumns; column++) {
+      int bottom = numRows - 1;
+      int numStones = 0;
+      for (int row = numRows - 1; row >=0; row--) {
+        // Count the number of stones until we reach an obstacle or the top
+        switch(matrix[row][column]) {
+          case STONE:
+            numStones++;
             break;
-          case '#':
-            currentTop = y - 1;
+          case EMPTY:
             break;
-          default:
-            if (y != currentTop) {
-              matrix[currentTop][x] = '.';
-              matrix[y][x] = ' ';
-              currentTop = currentTop - 1;
+          case OBSTACLE:
+            for (int row2 = bottom; row2 > row; row2--) {
+              if (numStones > 0) {
+                matrix[row2][column] = STONE;
+                numStones--;
+              } else {
+                matrix[row2][column] = EMPTY;
+              }
             }
+            bottom = row - 1;
+            break;
         }
-
+      }
+      // Fill in above the last obstacle
+      for (int row = bottom; row >=0; row--) {
+        if (numStones-- > 0) {
+          matrix[row][column] = STONE;
+        } else {
+          matrix[row][column] = EMPTY;
+        }
       }
     }
+
   }
 
-  static void printMatrix(char[][] matrix) {
-    System.out.println("---------");
-    for (int x = 0; x < matrix.length; x++) {
-      for (int y = 0; y< matrix[x].length; y++) {
-        System.out.print("|");
-        System.out.print(matrix[x][y]);
-      }
-      System.out.println("|");
-    }
-    System.out.println("---------");
-
-    return;
+  public char[][]rotateTheBox(char[][] box) {
+    char[][] retVal = rotate(box);
+    gravity(retVal);
+    return retVal;
   }
 
   public static void main(String[] args) {
-    char[][]box = new char[5][3];
+    char[][]box = new char[][]{{STONE, EMPTY, STONE}};
+    System.out.println(Arrays.deepToString(box));
+    System.out.println(Arrays.deepToString(new RotatingTheBox().rotateTheBox(box)));
 
-    box[0] = new char[]{'a', 'b', 'c'};
-    box[1] = new char[]{'d', 'e', 'f'};
-    box[2] = new char[]{'g', 'h', 'i'};
-    box[3] = new char[]{'j', 'k', 'l'};
-    box[4] = new char[]{'m', 'n', 'o'};
+    box = new char[][]{{STONE, EMPTY, OBSTACLE, EMPTY}, {STONE, STONE, OBSTACLE, EMPTY}};
+    System.out.println(Arrays.deepToString(box));
+    System.out.println(Arrays.deepToString(new RotatingTheBox().rotateTheBox(box)));
 
-    System.out.println("box[2][3]: " + box[3][2]);
-    System.out.println("Original:");
-    printMatrix(box);
-    System.out.println("Rotated:");
-    printMatrix(rotate(box));
+    box = new char[][]{{STONE, EMPTY, OBSTACLE, EMPTY, STONE, EMPTY}};
+    System.out.println(Arrays.deepToString(box));
+    System.out.println(Arrays.deepToString(new RotatingTheBox().rotateTheBox(box)));
+
   }
 }
